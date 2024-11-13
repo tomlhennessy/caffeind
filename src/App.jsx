c57e140d2540649ef8e91a61838ba5942ff8fe10
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-13 14:16:27 +1000

    hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Layout from './components/Layout'
 import Hero from './components/Hero'
+import CoffeeForm from './components/CoffeeForm'
+import Stats from './components/Stats'
+import History from './components/History'
 
 function App() {
   const isAuthenticated = false
 
+  const authenticatedContent = (
+    <>
+      <Stats />
+      <History />
+    </>
+  )
+
   return (
     <Layout>
       <Hero />
+      <CoffeeForm />
+      {isAuthenticated && (authenticatedContent)}
     </Layout>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,21 @@
 export default function Hero() {
     return (
         <>
-
+            <h1>Coffee tracking for coffee <abbr title="An enthusiast or devotee">Fiends</abbr>!</h1>
+            <div className="benefits-list">
+                <h3 className="font-bolder">try <span className="text-gradient">Caffeind</span> and start ...</h3>
+                <p>✅ Tracking every coffee</p>
+                <p>✅ Measuring your blood caffeine levels</p>
+                <p>✅ Costing and quantifying your addiction</p>
+            </div>
+            <div className="card info-card">
+                <div>
+                    <i className="fa-solid fa-circle-info"></i>
+                    <h3>Did you know...</h3>
+                    <h5>That caffeine&apos;s half-life is about 5 hours?</h5>
+                    <p>This means that after 5 hours, half the caffeine you consumed is still in your system, keeping you alert longer! So if you drink a cup of coffee with 200 mg of caffeine, about 5 hours later, you&apos;ll have about 100 mg of caffeine in your system.</p>
+                </div>
+            </div>
         </>
     )
 }
