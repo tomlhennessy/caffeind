3156d84e356d2213622673e7049aa40db8b7baab
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-13 12:03:27 +1000

    layout and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import Layout from './components/Layout'
+import Hero from './components/Hero'
 
 function App() {
+  const isAuthenticated = false
 
   return (
-    <>
-
-    </>
+    <Layout>
+      <Hero />
+    </Layout>
   )
 }
 
