8f987ad9891cb20b24f24f5b50b5f29f74696002
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-14 15:13:28 +1000

    history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,7 +1,31 @@
+import { coffeeConsumptionHistory, timeSinceConsumption, getCaffeineAmount, calculateCurrentCaffeineLevel } from '../utils'
+
 export default function History() {
     return (
         <>
+            <div className="section-header">
+                <i className="fa-solid fa-timeline"></i>
+                <h2>History</h2>
+            </div>
+            <p><i>Hover for more information!</i></p>
+            <div className="coffee-history">
+                {Object.keys(coffeeConsumptionHistory).sort((a, b) => b - a).map ((utcTime, coffeeIndex) => {
+                    const coffee = coffeeConsumptionHistory[utcTime]
+                    const timeSinceConsume = timeSinceConsumption(utcTime)
+                    const originalAmount = getCaffeineAmount(coffee.name)
+                    const remainingAmount = calculateCurrentCaffeineLevel({
+                        [utcTime]: coffee
+                    })
 
+                    const summary = `${coffee.name} | ${timeSinceConsume} | $${coffee.cost} | ${remainingAmount} mg / ${originalAmount} mg`
+
+                    return (
+                        <div title={summary} key={coffeeIndex}>
+                            <i className="fa-solid fa-mug-hot" />
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
